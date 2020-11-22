17ab39232fb456b03102b1648e1fabb868218526
rahul harlalka <rahulharlalka96@gmail.com>
2020-11-22 20:03:21 +0530

    final app made

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,8 +3,7 @@ import React from "react";
 function NavBar(props){
     return (
         <nav className="navbar navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar {props.totalCounters}</a>     
-            
+            <a className="navbar-brand" href="#">Navbar {props.totalCounters}</a>            
         </nav>
     );
 }
