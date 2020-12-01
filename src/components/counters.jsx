42b401c2668c904af0205a24428fd093bf4100ba
rahul harlalka <rahulharlalka96@gmail.com>
2020-12-01 12:14:06 +0530

    project completed

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,26 +1,27 @@
-import React,{useState} from 'react';
-import Counter from "./counter"
+import React, { useState } from "react";
+import Counter from "./counter";
 
-function Counters(props){    
-    return (
-        <div>
-            
-            {props.counters.map(counter=>(
-                <Counter 
-                    key={counter.id} 
-                    id={counter.id}
-                    value={counter.value}
-                    onDelete={props.onDelete} 
-                    onDecrement={props.onDecrement}
-                    onIncrement={props.onIncrement}
-                    counter={counter}
-                >
-                    <h4>Counter {counter.id}</h4>
-                </Counter>
-            ))}
-            <button onClick={props.onReset} className="btn btn-danger btn-lg">Reset</button>
-        </div>
-    )
+function Counters(props) {
+  return (
+    <div>
+      {props.counters.map((counter) => (
+        <Counter
+          key={counter.id}
+          id={counter.id}
+          value={counter.value}
+          onDelete={props.onDelete}
+          onDecrement={props.onDecrement}
+          onIncrement={props.onIncrement}
+          counter={counter}
+        >
+          <h4>Counter {counter.id}</h4>
+        </Counter>
+      ))}
+      <button onClick={props.onReset} className="btn btn-danger btn-lg">
+        Reset
+      </button>
+    </div>
+  );
 }
 
 export default Counters;
